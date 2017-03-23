13fc840e219ec3ae8a85028f30e9098d02a60d78
Andrew Smith <andrw.l.smth@gmail.com>
2017-03-22 23:51:34 -0400

    Make switch states persistent

--- Android/RPiPowerStrip/app/src/main/java/com/example/rpipowerstrip/MainActivity.java
+++ Android/RPiPowerStrip/app/src/main/java/com/example/rpipowerstrip/MainActivity.java
@@ -32,6 +32,15 @@ public class MainActivity extends AppCompatActivity
         mUser = settings.getString("user", "");
         mPass = settings.getString("pass", "");
 
+        boolean mSwitchState1 = settings.getBoolean("ss1", false);
+        boolean mSwitchState2 = settings.getBoolean("ss2", false);
+        boolean mSwitchState3 = settings.getBoolean("ss3", false);
+        boolean mSwitchState4 = settings.getBoolean("ss4", false);
+        boolean mSwitchState5 = settings.getBoolean("ss5", false);
+        boolean mSwitchState6 = settings.getBoolean("ss6", false);
+        boolean mSwitchState7 = settings.getBoolean("ss7", false);
+        boolean mSwitchState8 = settings.getBoolean("ss8", false);
+
         class CheckChangeListener implements CompoundButton.OnCheckedChangeListener
         {
             private String mChannel;
@@ -46,8 +55,14 @@ public class MainActivity extends AppCompatActivity
             {
                 if (mHost != null && mUser != null && mPass != null)
                 {
+                    String prefName = "ss" + mChannel;
+
                     if (isChecked)
                     {
+                        SharedPreferences.Editor editor = getSharedPreferences(PREFERENCES, 0).edit();
+                        editor.putBoolean(prefName, true);
+                        editor.apply();
+
                         String command = "cd RelayControl/ && python relay_controller_android.py " + mChannel + " 0";
                         mConnectionManager = new ConnectionManager(mHost, mUser, mPass, command, new ConnectionManager.AsyncResponse(){
 
@@ -59,7 +74,12 @@ public class MainActivity extends AppCompatActivity
                             }
                         });
                         mConnectionManager.execute();
-                    } else {
+                    }
+                    else
+                    {
+                        SharedPreferences.Editor editor = getSharedPreferences(PREFERENCES, 0).edit();
+                        editor.putBoolean(prefName, true);
+                        editor.apply();
 
                         String command = "cd RelayControl/ && python relay_controller_android.py " + mChannel + " 1";
                         mConnectionManager = new ConnectionManager(mHost, mUser, mPass, command, new ConnectionManager.AsyncResponse(){
@@ -78,35 +98,35 @@ public class MainActivity extends AppCompatActivity
         }
 
         Switch switch1 = (Switch) findViewById(R.id.switch1);
-        switch1.setChecked(false);
+        switch1.setChecked(mSwitchState1);
         switch1.setOnCheckedChangeListener(new CheckChangeListener("1"));
 
         Switch switch2 = (Switch) findViewById(R.id.switch2);
-        switch2.setChecked(false);
+        switch2.setChecked(mSwitchState2);
         switch2.setOnCheckedChangeListener(new CheckChangeListener("2"));
 
         Switch switch3 = (Switch) findViewById(R.id.switch3);
-        switch3.setChecked(false);
+        switch3.setChecked(mSwitchState3);
         switch3.setOnCheckedChangeListener(new CheckChangeListener("3"));
 
         Switch switch4 = (Switch) findViewById(R.id.switch4);
-        switch4.setChecked(false);
+        switch4.setChecked(mSwitchState4);
         switch4.setOnCheckedChangeListener(new CheckChangeListener("4"));
 
         Switch switch5 = (Switch) findViewById(R.id.switch5);
-        switch5.setChecked(false);
+        switch5.setChecked(mSwitchState5);
         switch5.setOnCheckedChangeListener(new CheckChangeListener("5"));
 
         Switch switch6 = (Switch) findViewById(R.id.switch6);
-        switch6.setChecked(false);
+        switch6.setChecked(mSwitchState6);
         switch6.setOnCheckedChangeListener(new CheckChangeListener("6"));
 
         Switch switch7 = (Switch) findViewById(R.id.switch7);
-        switch7.setChecked(false);
+        switch7.setChecked(mSwitchState7);
         switch7.setOnCheckedChangeListener(new CheckChangeListener("7"));
 
         Switch switch8 = (Switch) findViewById(R.id.switch8);
-        switch8.setChecked(false);
+        switch8.setChecked(mSwitchState8);
         switch8.setOnCheckedChangeListener(new CheckChangeListener("8"));
     }
 
